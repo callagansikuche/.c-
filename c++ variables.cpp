3823e68c21e7bfb7881bfb3e7cea2947bf8a44fe
sikuchecallagan <158299250+callagansikuche@users.noreply.github.com>
2024-03-29 14:16:09 +0300

    Add files via upload

---
+++ c++ variables.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int myNum= 1000;
+    cout<<myNum<<endl;
+    float myNume =5.65;
+    cout<<myNume<<endl;
+    double myNumm =20.543;
+    cout<<myNumm<<endl;
+    /*float can have up to 6/7 decimal digits
+     while double variables have up to 15 digits*/
+        float f1=35e3;
+        double d1 =54E2;
+        cout<<f1 <<endl;
+        cout<<d1<<endl;
+        bool isCodingFun= true;
+        bool isFishTasty= false;
+        cout<<isCodingFun<<endl;
+        cout<<isFishTasty<<endl;
+        char myGrade='B';
+        cout<<myGrade<<endl;
+        string greeting="Hello";
+        cout<<greeting<<endl;
+/*string uses double quotation character uses single quotation*/
+    return 0;
+}
